c5307e81dbec06b4ea4cb96aa6959f9c08c8d4c9
anthony <116324140+AnthonyQuispeHuamani@users.noreply.github.com>
2022-12-12 22:14:17 -0500

    archivo ignorado

--- src/main/java/com/servlets/CitaListServlet.java
+++ src/main/java/com/servlets/CitaListServlet.java
@@ -82,7 +82,7 @@ public class CitaListServlet extends HttpServlet {
       System.out.println(theException);
     }
 
-    }
+}
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
